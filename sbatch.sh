29f940453ccc7d54fa16867cab4969fcf426ac79
Marsboy02 <marsboy0619@gmail.com>
2025-01-18 14:38:45 +0900

    fix: sbatch script do not use unload

--- sbatch.sh
+++ sbatch.sh
@@ -1,8 +1,8 @@
 #!/bin/bash
 #SBATCH --nodes=1
-#SBATCH --partition=gpu4
-#SBATCH --gres=gpu:a6000:4
-#SBATCH --cpus-per-task=56
+#SBATCH --partition=gpu2
+#SBATCH --gres=gpu:a10:4
+#SBATCH --cpus-per-task=8
 #SBATCH --job-name=test_root
 #SBATCH -o ./results/jupyter.%N.%j.out  # STDOUT
 #SBATCH -e ./results_log/jupyter.%N.%j.err  # STDERR
@@ -12,7 +12,6 @@ echo "node: $HOSTNAME"
 echo "jobid: $SLURM_JOB_ID"
 
 # set slurm module
-module unload CUDA/11.2.2
 module load CUDA/11.2.2
 module add python/3.11.2
 
@@ -21,10 +20,11 @@ python3 -m venv venv
 source venv/bin/activate
 
 # install dependency
-pip3 install torch
 pip3 install pandas
-pip3 install transformers
+pip3 install torch
 pip3 install scikit-learn
+pip install --upgrade pip setuptools
+pip3 install transformers
 pip3 install numpy
 
 python3 train.py
